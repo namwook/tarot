a334439df1b02e406bfca181566c77ff037ce426
namwook <nameuk166@gmail.com>
2017-11-06 22:55:24 +0900

    그날 그날 유저가 섞은 랜덤 카드가 저장됨. User의 정보가 json format으로 저장되고 이 json file에서 card list를 뽑아내어 결과를 보여준다.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const info_1 = require("./info");
 exports.router = express.Router();
 exports.router.get('/', (req, res) => {
@@ -19,102 +20,74 @@ exports.router.post('/', (req, res) => {
     let randomFirstIndex;
     let randomSecIndex;
     let randomThirdIndex;
+    //test
+    let test = saveInfo(name, todayDate);
+    console.log(test);
     //FIXME: 지금은 list로 전해져오지만 json을 통해서 list를 반환받아야함
     list = userInfoSearch(name, todayDate);
     console.log(`return list : ${list}`);
     randomFirstIndex = list[fst];
     randomSecIndex = list[sec];
     randomThirdIndex = list[third];
-    const tarotContent = jsonRead();
+    const tarotContent = jsonRead("./public/json/tarot.json");
     let f_name = tarotContent.tarotName[randomFirstIndex];
     let s_name = tarotContent.tarotName[randomSecIndex];
     let t_name = tarotContent.tarotName[randomThirdIndex];
     let f_content = tarotContent.tarotMeaning[randomFirstIndex];
     let s_content = tarotContent.tarotMeaning[randomSecIndex];
     let t_content = tarotContent.tarotMeaning[randomThirdIndex];
+    let resultTarot = jsonRead("./public/json/result.json");
+    let result = resultTarot[getRandom(0, 3) % 3];
     res.render('users', {
         title: 'Today Tarot', name: name,
         past: f_content, present: s_content, future: t_content,
         past_name: f_name, present_name: s_name, future_name: t_name,
         img_past: '/images/' + f_name + '.jpg',
         img_present: '/images/' + s_name + '.jpg',
-        img_future: '/images/' + t_name + '.jpg'
+        img_future: '/images/' + t_name + '.jpg',
+        result: result
     });
 });
 //FIXME: User정보를 JSON으로 저장.
 function saveInfo(name, date) {
+    console.log('=====================save info======================');
     let list;
     let savedName = new Map();
-    //name을 매개로 date에 맞는 consCArd를 꺼내자
+    let userDataPath = path.join(__dirname, '..', 'public/userData');
     let sU = { id: name, date: date, constCard: [] };
-    // 저장된게 하나도 없으면 초기화
-    if (savedName.size == 0) {
-        let user = new info_1.User(name);
-        list = user.getCard(date);
-        savedName.set(name, [sU]);
-        savedName.get(name)[0].constCard = list;
-        // return list;
-    }
-    //이름 탐색
-    /*let c = false;
+    let user = new info_1.User(name);
+    list = user.getCard(date);
+    savedName.set(name, [sU]);
+    savedName.get(name)[0].constCard = list;
+    // User 정보를 'public/userData/현재 날짜' 디렉토리에 name.json 으로 저장한다
+    let userMapIter = savedName.keys();
     for (let i = 0; i < savedName.size; i++) {
-        if (savedName.get(name)[i].id == name) {
-            // FIXME: 정보를 Json에서 찾아야함.
-            // 이미 name이 있을 경우,
-            c = true;
-            list = savedName.get(name)[0].constCard;
-            // return list;
-        }
+        let key = userMapIter.next().value;
+        let data = JSON.stringify(savedName.get(key)[i]);
+        let userDirName = path.join(userDataPath, savedName.get(name)[i].date);
+        let userFileName = path.join(userDirName, savedName.get(name)[i].id + '.json');
+        console.log(`data : ${data}`);
+        console.log(`user file name is : ${userFileName}`);
+        if (!fs.existsSync(userDirName))
+            fs.mkdirSync(userDirName);
+        if (!fs.existsSync(userFileName))
+            fs.writeFileSync(userFileName, data, 'utf8');
     }
-
-    if (!c) {
-        let user = new User(name);
-        list = user.getCard(date);
-        savedName.set(name, [sU]);
-        savedName.get(name)[0].constCard = list;
-        // return list;
-    }
-
-    let data = JSON.stringify(savedName.get(name)[0]);
-
-    //FIXME: writefilesync를 하면 연결을 재설정해야한다는 오류가난다... ㅠㅠ
-    // let fileJson = fs.writeFileSync("./public/json/userInfo.json", data, 'utf8');
-    */
 }
 function userInfoSearch(name, todayDate) {
-    //FIXME: User 정보 Json File로 정리해서 꺼내서 탐색.
-    let readJson = fs.readFileSync("./public/json/userInfo.json", 'utf8');
+    console.log('=====================userinfosearch======================');
+    let readJson = fs.readFileSync('./public/userData/' + todayDate + '/' + name + '.json', 'utf8');
     let rstCard = JSON.parse(readJson).constCard;
+    console.log(`readJson id is : ${JSON.parse(readJson).id}`);
+    console.log(`result card is : ${rstCard}`);
     return rstCard;
-}
-function randomCardO() {
-    let listBool = new Array(22);
-    let list = new Array();
-    let c = 0;
-    let index = 0;
-    listBool.fill(false, 0, 22);
-    while (c <= 21) {
-        index = 0;
-        let cardIndex = getRandom(0, listBool.length) % 23;
-        if (listBool[cardIndex] == false) {
-            list.push(cardIndex);
-            listBool[cardIndex] = true;
-            console.log(list);
-        }
-        c = 0;
-        for (let i in listBool) {
-            if (listBool[i] == true)
-                c++;
-        }
-    }
-    return list;
 }
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-function jsonRead() {
-    let fileJson = fs.readFileSync("./public/json/tarot.json", 'utf8');
-    const fileJsonContent = JSON.parse(fileJson);
-    return fileJsonContent;
+function jsonRead(path) {
+    let fileJson = fs.readFileSync(path, 'utf8');
+    const fileJContent = JSON.parse(fileJson);
+    return fileJContent;
 }
 //# sourceMappingURL=users.js.map
--- routes/users.ts
+++ routes/users.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
 import * as fs from 'fs';
 import {Request, Response} from "express";
+import * as path from 'path';
 import {User} from "./info";
+
 
 export const router = express.Router();
 
@@ -11,6 +13,7 @@ router.get('/', (req: Request, res: Response) => {
 
 //FIXME: 카드 자리 중복 입력시에 제출 금지 -> 수정..
 router.post('/', (req: Request, res: Response) => {
+
     let d = new Date();
     let todayDate = d.getFullYear().toString() + d.getMonth().toString() + d.getDate().toString();
 
@@ -23,15 +26,20 @@ router.post('/', (req: Request, res: Response) => {
     let randomSecIndex: number;
     let randomThirdIndex: number;
 
+    //test
+    let test = saveInfo(name, todayDate);
+    console.log(test);
+
     //FIXME: 지금은 list로 전해져오지만 json을 통해서 list를 반환받아야함
     list = userInfoSearch(name, todayDate);
+
     console.log(`return list : ${list}`);
 
     randomFirstIndex = list[fst];
     randomSecIndex = list[sec];
     randomThirdIndex = list[third];
 
-    const tarotContent = jsonRead();
+    const tarotContent = jsonRead("./public/json/tarot.json");
 
     let f_name = tarotContent.tarotName[randomFirstIndex];
     let s_name = tarotContent.tarotName[randomSecIndex];
@@ -41,6 +49,8 @@ router.post('/', (req: Request, res: Response) => {
     let s_content = tarotContent.tarotMeaning[randomSecIndex];
     let t_content = tarotContent.tarotMeaning[randomThirdIndex];
 
+    let resultTarot = jsonRead("./public/json/result.json");
+    let result = resultTarot[getRandom(0, 3) % 3];
 
     res.render('users', {
         title: 'Today Tarot', name: name,
@@ -48,93 +58,65 @@ router.post('/', (req: Request, res: Response) => {
         past_name: f_name, present_name: s_name, future_name: t_name,
         img_past: '/images/' + f_name + '.jpg',
         img_present: '/images/' + s_name + '.jpg',
-        img_future: '/images/' + t_name + '.jpg'
-    });
+        img_future: '/images/' + t_name + '.jpg',
+        result: result
+    })
+    ;
 
 });
 
 //FIXME: User정보를 JSON으로 저장.
 function saveInfo(name: string, date: string) {
+    console.log('=====================save info======================');
     let list: number[];
     type savedUser = { id: string, date: string, constCard: number[] };
     let savedName: Map<string, savedUser[]> = new Map<string, savedUser[]>();
-
-    //name을 매개로 date에 맞는 consCArd를 꺼내자
+    let userDataPath = path.join(__dirname, '..', 'public/userData');
     let sU: savedUser = {id: name, date: date, constCard: []};
 
-    // 저장된게 하나도 없으면 초기화
-    if (savedName.size == 0) {
-        let user = new User(name);
-        list = user.getCard(date);
-        savedName.set(name, [sU]);
-        savedName.get(name)[0].constCard = list;
-        // return list;
-    }
+    let user = new User(name);
+    list = user.getCard(date);
+    savedName.set(name, [sU]);
+    savedName.get(name)[0].constCard = list;
 
-    //이름 탐색
-    /*let c = false;
+    // User 정보를 'public/userData/현재 날짜' 디렉토리에 name.json 으로 저장한다
+    let userMapIter = savedName.keys();
+
     for (let i = 0; i < savedName.size; i++) {
-        if (savedName.get(name)[i].id == name) {
-            // FIXME: 정보를 Json에서 찾아야함.
-            // 이미 name이 있을 경우,
-            c = true;
-            list = savedName.get(name)[0].constCard;
-            // return list;
-        }
+        let key = userMapIter.next().value;
+        let data = JSON.stringify(savedName.get(key)[i]);
+        let userDirName = path.join(userDataPath, savedName.get(name)[i].date);
+        let userFileName = path.join(userDirName, savedName.get(name)[i].id + '.json');
+
+        console.log(`data : ${data}`);
+        console.log(`user file name is : ${userFileName}`);
+
+        if (!fs.existsSync(userDirName)) fs.mkdirSync(userDirName);
+        if (!fs.existsSync(userFileName)) fs.writeFileSync(userFileName, data, 'utf8');
     }
-
-    if (!c) {
-        let user = new User(name);
-        list = user.getCard(date);
-        savedName.set(name, [sU]);
-        savedName.get(name)[0].constCard = list;
-        // return list;
-    }
-
-    let data = JSON.stringify(savedName.get(name)[0]);
-
-    //FIXME: writefilesync를 하면 연결을 재설정해야한다는 오류가난다... ㅠㅠ
-    // let fileJson = fs.writeFileSync("./public/json/userInfo.json", data, 'utf8');
-    */
 }
 
 
 function userInfoSearch(name: string, todayDate: string) {
-    //FIXME: User 정보 Json File로 정리해서 꺼내서 탐색.
-    let readJson = fs.readFileSync("./public/json/userInfo.json", 'utf8');
-    let rstCard : number[] = JSON.parse(readJson).constCard;
+    console.log('=====================userinfosearch======================');
+
+    let readJson = fs.readFileSync('./public/userData/' + todayDate + '/' + name + '.json', 'utf8');
+    let rstCard: number[] = JSON.parse(readJson).constCard;
+
+    console.log(`readJson id is : ${JSON.parse(readJson).id}`);
+    console.log(`result card is : ${rstCard}`);
+
     return rstCard;
 }
 
-function randomCardO() {
-    let listBool = new Array(22);
-    let list = new Array();
-    let c: number = 0;
-    let index: number = 0;
-    listBool.fill(false, 0, 22);
-    while (c <= 21) {
-        index = 0;
-        let cardIndex = getRandom(0, listBool.length) % 23;
-        if (listBool[cardIndex] == false) {
-            list.push(cardIndex);
-            listBool[cardIndex] = true;
-            console.log(list);
-        }
-        c = 0;
-        for (let i in listBool) {
-            if (listBool[i] == true) c++;
-        }
-    }
-    return list;
-}
 
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function jsonRead() {
-    let fileJson = fs.readFileSync("./public/json/tarot.json", 'utf8');
-    const fileJsonContent = JSON.parse(fileJson);
-    return fileJsonContent;
+function jsonRead(path: string) {
+    let fileJson = fs.readFileSync(path, 'utf8');
+    const fileJContent = JSON.parse(fileJson);
+    return fileJContent;
 }
 
